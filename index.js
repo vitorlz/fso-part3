afebd120d77998ac9e9e76a2393c1fa9aee6fcf9
vitorlz <vitorlzr0901@gmail.com>
2024-08-15 13:59:21 -0300

    buildui

--- index.js
+++ index.js
@@ -9,103 +9,103 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
 
-morgan.token('data', (req, res) => {
-    return JSON.stringify(req.body)
+morgan.token('data', (req) => {
+  return JSON.stringify(req.body)
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
-app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+app.get('/api/persons', (request, response, next) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
     .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
-    Person.find({}).then(persons => {
-        response.send(
-            `<p>The phonebook has info for ${persons.length} people</p>
+app.get('/info', (request, response, next) => {
+  Person.find({}).then(persons => {
+    response.send(
+      `<p>The phonebook has info for ${persons.length} people</p>
             <p>${new Date()}</p>`
-        )
-    })
+    )
+  })
     .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
     
-    Person.findById(request.params.id)
-        .then(person => {
-            if (person) {
-                response.json(person)
-            } else{
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else{
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
     
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
     
-    Person.findByIdAndDelete(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const person = new Person({
-        name: body.name,
-        number: body.number
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
     })
-
-    person.save()
-        .then(savedPerson => {
-            response.json(savedPerson)
-        })
-        .catch(error => next(error))
+    .catch(error => next(error))
 
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number
-    }
+  const person = {
+    name: body.name,
+    number: body.number
+  }
     
-    Person.findByIdAndUpdate(
-        request.params.id, 
-        person, 
-        { new: true, runValidators: true }
-    )
-        .then(updatedPerson => {
-            response.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  Person.findByIdAndUpdate(
+    request.params.id, 
+    person, 
+    { new: true, runValidators: true }
+  )
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint'})
+  response.status(404).send({ error: 'unknown endpoint'})
 }
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message)
+  console.log(error.message)
     
-    if(error.name === 'CastError') {
-        response.status(400).send({ error: 'malformated id' })
-    } else if (error.name === 'ValidationError') {
-        response.status(400).send({ error: error.message })
-    }
+  if(error.name === 'CastError') {
+    response.status(400).send({ error: 'malformated id' })
+  } else if (error.name === 'ValidationError') {
+    response.status(400).send({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(errorHandler)
@@ -113,6 +113,6 @@ app.use(errorHandler)
 const PORT = process.env.PORT
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
 
--- models/person.js
+++ models/person.js
@@ -3,39 +3,39 @@ const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
-    .then(result => {
-        console.log("Connected to MongoDB")
-    })
-    .catch(error => {
-        console.log("Error connecting to database")
-    })
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch(() => {
+    console.log('Error connecting to database')
+  })
 
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        minLength: 8,
-        required: true,
-        validate: {
-            validator: (v) => {
-                return /^\d{2,3}-\d+$/.test(v)
-            },
-            message: `This phone number is not valid`
-        }
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: (v) => {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: 'This phone number is not valid'
     }
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
